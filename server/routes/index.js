8f74cd17d2fc1e1943ad98995a9bae06a551013b
brandonnegen <bnegen@gmail.com>
2015-08-06 13:50:22 -0500

    Changes one ajax to three

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var router = express.Router();//Router has to be capitalized
 var path = require('path');
-var wordList = require('../public/data/words.json')
+var firstAdj = require('../public/data/adj1.json');
+var secondAdj = require('../public/data/adj2.json');
+var noun = require('../public/data/noun.json');
 
-router.get("/data", function(req, res){
-    res.json(wordList);
+router.get("/adj1", function(req, res){
+    res.json(firstAdj);
+});
+
+router.get("/adj2", function(req, res){
+    res.json(secondAdj);
+});
+
+router.get("/noun", function(req, res){
+    res.json(noun);
 });
 
 router.get("/*", function(req, res){
